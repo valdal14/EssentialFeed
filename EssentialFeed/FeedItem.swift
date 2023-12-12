e4b65df8be5de97bea2416aaa6e630af63f89841
Valerio DAlessio <u105550@canda.com>
2023-12-12 12:38:56 +0100

    created the FeedItem

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Valerio D'ALESSIO on 12/12/23.
+//
+
+import Foundation
+
+struct FeedItem {
+   let id: UUID
+   let description: String?
+   let location: String?
+   let imageURL: URL
+}
