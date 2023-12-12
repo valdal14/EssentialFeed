970594ffea05977586f54f0b623afcc61b27db30
Valerio DAlessio <u105550@canda.com>
2023-12-12 16:22:18 +0100

    delivers invalid data on http 200 response with invalid json

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -47,7 +47,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
 		XCTAssertEqual(capturedErrors, [.connectivity])
 	}
 	
-	func test_load_deliverErrorOnNon200HttpResponse() {
+	func test_load_deliversErrorOnNon200HttpResponse() {
 		let (sut, client) = makeSUT()
 		
 		let httpStatusCodes = [199, 201, 300, 400, 500]
@@ -60,6 +60,19 @@ final class RemoteFeedLoaderTests: XCTestCase {
 			
 			XCTAssertEqual(capturedErrors, [.invalidData])
 		}
+	}
+	
+	func test_load_deliversErrorOn200HTTPResponseWithInvalidJSON() {
+		let (sut, client) = makeSUT()
+		
+		var capturedErrors = [RemoteFeedLoader.Error]()
+		sut.load { capturedErrors.append($0) }
+		
+		let invalidJSON = "invalid json".data(using: .utf8)!
+		
+		client.complete(withStatusCode: 200, data: invalidJSON)
+		
+		XCTAssertEqual(capturedErrors, [.invalidData])
 	}
 	
 	// MARK: - Helpers
@@ -79,13 +92,13 @@ final class RemoteFeedLoaderTests: XCTestCase {
 			messages[index].completion(.failure(error))
 		}
 		
-		func complete(withStatusCode code: Int, at index: Int = 0) {
+		func complete(withStatusCode code: Int, data: Data = Data(), at index: Int = 0) {
 			let response = HTTPURLResponse(url: requestedURLs[index],
 										   statusCode: code,
 										   httpVersion: nil,
 										   headerFields: nil)!
 			
-			messages[index].completion(.success(response))
+			messages[index].completion(.success(data, response))
 		}
 	}
 	
