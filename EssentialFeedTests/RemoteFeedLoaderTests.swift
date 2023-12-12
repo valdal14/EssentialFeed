b2d598e4441ea49b5e4f3b9b0debd7d8817a0bd7
Valerio DAlessio <u105550@canda.com>
2023-12-12 16:05:53 +0100

    added a type as part of the HTTPClient completion

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -65,18 +65,18 @@ final class RemoteFeedLoaderTests: XCTestCase {
 	// MARK: - Helpers
 	
 	private class HTTPClientSpy: HTTPClient {
-		var messages: [(url: URL, completion: ((Error?, HTTPURLResponse?) -> Void))] = []
+		var messages: [(url: URL, completion: ((HTTPClientResult) -> Void))] = []
 		
 		var requestedURLs: [URL] {
 			return messages.compactMap { $0.url }
 		}
 		
-		func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void) {
+		func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
 			messages.append((url: url, completion: completion))
 		}
 		
 		func complete(with error: Error, at index: Int = 0) {
-			messages[index].completion(error, nil)
+			messages[index].completion(.failure(error))
 		}
 		
 		func complete(withStatusCode code: Int, at index: Int = 0) {
@@ -85,7 +85,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
 										   httpVersion: nil,
 										   headerFields: nil)!
 			
-			messages[index].completion(nil, response)
+			messages[index].completion(.success(response))
 		}
 	}
 	
