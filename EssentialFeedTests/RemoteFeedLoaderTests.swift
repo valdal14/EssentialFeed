f1f7fd5b075cd3e068326259279d5cb28f80ce0c
Valerio DAlessio <u105550@canda.com>
2023-12-12 14:27:53 +0100

    test multiple requests

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -12,16 +12,26 @@ final class RemoteFeedLoaderTests: XCTestCase {
 	
 	func test_init_doesNotRequestDataFromURL() {
 		let (_, client) = makeSUT()
-		XCTAssertNil(client.requestedURL, "requestedURL was not nil")
+		XCTAssertTrue(client.requestedURLs.isEmpty, "requestedURL was not empty")
 	}
 	
 	func test_load_requestsDataFromURL() {
-		let givenURL = URL(string: "http://given-url.com")!
+		let url = URL(string: "http://given-url.com")!
 		
 		let (sut, client) = makeSUT()
 		sut.load()
 		
-		XCTAssertEqual(client.requestedURL, givenURL, "Given url \(givenURL) does not match the requestedURL \(String(describing: client.requestedURL))")
+		XCTAssertEqual(client.requestedURLs, [url], "Given url \(url) does not match the requestedURL \(client.requestedURLs)")
+	}
+	
+	func test_loadTwice_requestsDataFromURLTwice() {
+		let url = URL(string: "http://given-url.com")!
+		
+		let (sut, client) = makeSUT()
+		sut.load()
+		sut.load()
+		
+		XCTAssertEqual(client.requestedURLs, [url, url], "Expected 2 but got \(client.requestedURLs.count)")
 	}
 	
 	// MARK: - Helpers
@@ -32,10 +42,10 @@ final class RemoteFeedLoaderTests: XCTestCase {
 	}
 	
 	private class HTTPClientSpy: HTTPClient {
-		var requestedURL: URL?
+		var requestedURLs: [URL] = []
 		
 		func get(from url: URL) {
-			requestedURL = url
+			requestedURLs.append(url)
 		}
 	}
 }
