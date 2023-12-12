bb281606491ef64c1025dad001b642b8f5d5bbbc
Valerio DAlessio <u105550@canda.com>
2023-12-12 13:33:40 +0100

    make the URL a dependency injected to the RemoteFeedLoader

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -11,12 +11,14 @@ import XCTest
 // MARK: -  PROD Code
 class RemoteFeedLoader {
 	private let client: HTTPClient
+	private let url: URL
 	
-	init(client: HTTPClient) {
+	init(client: HTTPClient, url: URL) {
 		self.client = client
+		self.url = url
 	}
 	
-	func load(url: URL) {
+	func load() {
 		client.get(from: url)
 	}
 }
@@ -26,7 +28,7 @@ protocol HTTPClient {
 }
 
 
-// MARK: - Spy 
+// MARK: - Spy
 
 class HTTPClientSpy: HTTPClient {
 	var requestedURL: URL?
@@ -42,21 +44,21 @@ final class RemoteFeedLoaderTests: XCTestCase {
 	
 	func test_init_doesNotRequestDataFromURL() {
 		let (_, client) = makeSUT()
-		
 		XCTAssertNil(client.requestedURL, "requestedURL was not nil")
 	}
 	
 	func test_load_requestDataFromURL() {
 		let (sut, client) = makeSUT()
-		
-		sut.load(url: URL(string: "http://any-url.com")!)
+		sut.load()
 		
 		XCTAssertNotNil(client.requestedURL, "requestedURL was nil")
 	}
 	
 	// MARK: - Helpers
 	func makeSUT() -> (RemoteFeedLoader, HTTPClientSpy) {
+		let url = URL(string: "http://any-url.com")!
+		
 		let client = HTTPClientSpy()
-		return (RemoteFeedLoader(client: client), client)
+		return (RemoteFeedLoader(client: client, url: url), client)
 	}
 }
