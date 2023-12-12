7f3ee4df7e00bb5a2e76e293b62d458cacf7d37c
Valerio DAlessio <u105550@canda.com>
2023-12-12 14:12:42 +0100

    make the necessary classes and protocols public

--- EssentialFeed/FeedAPI/RemoteFeedLoader.swift
+++ EssentialFeed/FeedAPI/RemoteFeedLoader.swift
@@ -7,20 +7,20 @@
 
 import Foundation
 
-protocol HTTPClient {
+public protocol HTTPClient {
 	func get(from url: URL)
 }
 
-class RemoteFeedLoader {
+public class RemoteFeedLoader {
 	private let url: URL
 	private let client: HTTPClient
 	
-	init(url: URL, client: HTTPClient) {
+	public init(url: URL, client: HTTPClient) {
 		self.url = url
 		self.client = client
 	}
 	
-	func load() {
+	public func load() {
 		client.get(from: url)
 	}
 }
