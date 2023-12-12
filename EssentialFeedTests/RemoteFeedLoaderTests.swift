843a0b899040ab83cda74a5d7d6dd2d181700d20
Valerio DAlessio <u105550@canda.com>
2023-12-12 13:43:52 +0100

    modified the makeSUT to take a URL

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -9,13 +9,14 @@ import XCTest
 @testable import EssentialFeed
 
 // MARK: -  PROD Code
+
 class RemoteFeedLoader {
-	private let client: HTTPClient
 	private let url: URL
+	private let client: HTTPClient
 	
-	init(client: HTTPClient, url: URL) {
-		self.client = client
+	init(url: URL, client: HTTPClient) {
 		self.url = url
+		self.client = client
 	}
 	
 	func load() {
@@ -28,16 +29,6 @@ protocol HTTPClient {
 }
 
 
-// MARK: - Spy
-
-class HTTPClientSpy: HTTPClient {
-	var requestedURL: URL?
-	
-	func get(from url: URL) {
-		requestedURL = url
-	}
-}
-
 // MARK: - Test
 
 final class RemoteFeedLoaderTests: XCTestCase {
@@ -48,17 +39,26 @@ final class RemoteFeedLoaderTests: XCTestCase {
 	}
 	
 	func test_load_requestDataFromURL() {
+		let givenURL = URL(string: "http://given-url.com")!
+		
 		let (sut, client) = makeSUT()
 		sut.load()
 		
-		XCTAssertNotNil(client.requestedURL, "requestedURL was nil")
+		XCTAssertEqual(client.requestedURL, givenURL, "Given url \(givenURL) does not match the requestedURL \(String(describing: client.requestedURL))")
 	}
 	
 	// MARK: - Helpers
-	func makeSUT() -> (RemoteFeedLoader, HTTPClientSpy) {
-		let url = URL(string: "http://any-url.com")!
-		
+	
+	private func makeSUT(url: URL = URL(string: "http://given-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
 		let client = HTTPClientSpy()
-		return (RemoteFeedLoader(client: client, url: url), client)
+		return (RemoteFeedLoader(url: url, client: client), client)
+	}
+	
+	private class HTTPClientSpy: HTTPClient {
+		var requestedURL: URL?
+		
+		func get(from url: URL) {
+			requestedURL = url
+		}
 	}
 }
