76995d7d7315d65defa2e489fe4bfabedf89bde6
Valerio DAlessio <u105550@canda.com>
2023-12-16 15:55:39 +0100

    Extracted AUT creation to a factory method to protect against breaking changes

--- EssentialFeedTests/FeedCache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/FeedCache/CacheFeedUseCaseTests.swift
@@ -38,7 +38,6 @@ final class CacheFeedUseCaseTests: XCTestCase {
 	
 	/// delete the old cache
 	func test_init_doesNotDeleteCacheUponCreation() {
-		let store = FeedStore()
 		/**
 		 Here we are not adding any framework details like Codable
 		 or CoreData managed context parameters. By test-driving the
@@ -46,15 +45,14 @@ final class CacheFeedUseCaseTests: XCTestCase {
 		 to talk with this store. Then it may be implemented by any specific
 		 framework.
 		 */
-		_ = LocalFeedLoader(store: store)
+		let(_, store) = makeSUT()
 		// Assert we did not delete the cache upon creation
 		XCTAssertEqual(store.deleteCacheFeedCallCount, 0)
 	}
 
 	func test_save_requestsCacheDeletion() {
-		let store = FeedStore()
-		let sut = LocalFeedLoader(store: store)
 		let items: [FeedItem] = [uniqueItem(), uniqueItem()]
+		let (sut, store) = makeSUT()
 		
 		sut.save(items)
 		
@@ -62,6 +60,14 @@ final class CacheFeedUseCaseTests: XCTestCase {
 	}
 	
 	// MARK: - Helpers
+	private func makeSUT() -> (sut: LocalFeedLoader, store: FeedStore) {
+		let store = FeedStore()
+		let sut = LocalFeedLoader(store: store)
+		trackForMemoryLeak(store)
+		trackForMemoryLeak(sut)
+		return (sut, store)
+	}
+	
 	private func uniqueItem() -> FeedItem {
 		return FeedItem(
 			id: .init(),
