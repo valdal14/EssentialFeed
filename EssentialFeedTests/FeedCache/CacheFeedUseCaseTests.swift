7d0a936dbaba43c98c847eb48213f252e27ab3ed
Valerio DAlessio <u105550@canda.com>
2023-12-16 15:51:10 +0100

    Save command request cache deletetion

--- EssentialFeedTests/FeedCache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/FeedCache/CacheFeedUseCaseTests.swift
@@ -5,7 +5,9 @@
 //  Created by Valerio D'ALESSIO on 16/12/23.
 //
 
+import EssentialFeed
 import XCTest
+
 
 /**
  Helper class rapresenting the framework side and help defines
@@ -14,6 +16,10 @@ import XCTest
  */
 class FeedStore {
 	var deleteCacheFeedCallCount: Int = 0
+	
+	func deleteCachedFeed() {
+		deleteCacheFeedCallCount += 1
+	}
 }
 
 class LocalFeedLoader {
@@ -22,6 +28,10 @@ class LocalFeedLoader {
 	init(store: FeedStore) {
 		self.store = store
 	}
+	
+	func save(_ items: [FeedItem]) {
+		self.store.deleteCachedFeed()
+	}
 }
 
 final class CacheFeedUseCaseTests: XCTestCase {
@@ -29,9 +39,39 @@ final class CacheFeedUseCaseTests: XCTestCase {
 	/// delete the old cache
 	func test_init_doesNotDeleteCacheUponCreation() {
 		let store = FeedStore()
+		/**
+		 Here we are not adding any framework details like Codable
+		 or CoreData managed context parameters. By test-driving the
+		 implementation we endup with the interfaces on how we are going
+		 to talk with this store. Then it may be implemented by any specific
+		 framework.
+		 */
 		_ = LocalFeedLoader(store: store)
 		// Assert we did not delete the cache upon creation
 		XCTAssertEqual(store.deleteCacheFeedCallCount, 0)
 	}
 
+	func test_save_requestsCacheDeletion() {
+		let store = FeedStore()
+		let sut = LocalFeedLoader(store: store)
+		let items: [FeedItem] = [uniqueItem(), uniqueItem()]
+		
+		sut.save(items)
+		
+		XCTAssertEqual(store.deleteCacheFeedCallCount, 1)
+	}
+	
+	// MARK: - Helpers
+	private func uniqueItem() -> FeedItem {
+		return FeedItem(
+			id: .init(),
+			description: nil,
+			location: nil,
+			imageURL: anyURL()
+		)
+	}
+	
+	private func anyURL() -> URL {
+		return URL(string: "http://any-url.com")!
+	}
 }
