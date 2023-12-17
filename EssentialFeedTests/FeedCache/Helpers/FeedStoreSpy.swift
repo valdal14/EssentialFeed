5951c345782b2cd36a104de6e17d07064de4800b
Valerio DAlessio <u105550@canda.com>
2023-12-17 17:28:56 +0100

    load command request cache retrieval

--- EssentialFeedTests/FeedCache/Helpers/FeedStoreSpy.swift
+++ EssentialFeedTests/FeedCache/Helpers/FeedStoreSpy.swift
@@ -14,6 +14,7 @@ internal class FeedStoreSpy: FeedStore {
 	enum ReceivedMessage: Equatable {
 		case deleteCacheFeed
 		case insert([LocalFeedImage], Date)
+		case retrieve
 	}
 	
 	private(set) var receivedMessages: [ReceivedMessage] = []
@@ -45,5 +46,9 @@ internal class FeedStoreSpy: FeedStore {
 	
 	func completeInsertionSuccessfully(at index: Int = 0) {
 		insertionCompletions[index](nil)
+	}
+	
+	func retrieve() {
+		receivedMessages.append(.retrieve)
 	}
 }
--- EssentialFeedTests/FeedCache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeedTests/FeedCache/LoadFeedFromCacheUseCaseTests.swift
@@ -18,6 +18,14 @@ final class LoadFeedFromCacheUseCaseTests: XCTestCase {
 		}
 	}
 	
+	func test_load_requestCacheRetrieval() {
+		let(sut, store) = makeSUT()
+		
+		sut.load()
+		
+		XCTAssertEqual(store.receivedMessages, [.retrieve])
+	}
+	
 	// MARK: - Helpers
 	private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
 		let store = FeedStoreSpy()
