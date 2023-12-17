3877255884c55c1c5b6f5d71763f20fdf50f0038
Valerio DAlessio <u105550@canda.com>
2023-12-17 09:00:45 +0100

    Guarantee that the LocalFeedLoader does not deliver deletion error after instance has been deallocated

--- EssentialFeedTests/FeedCache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/FeedCache/CacheFeedUseCaseTests.swift
@@ -19,7 +19,8 @@ class LocalFeedLoader {
 	}
 	
 	func save(_ items: [FeedItem], completion: @escaping (Error?) -> Void) {
-		self.store.deleteCachedFeed { [unowned self] error in
+		self.store.deleteCachedFeed { [weak self] error in
+			guard let self = self else { return }
 			if error == nil {
 				/// here we can pass just the completion since they have the same signature
 				self.store.insert(items, timestamp: self.currentDate(), completion: completion)
@@ -121,6 +122,26 @@ final class CacheFeedUseCaseTests: XCTestCase {
 			store.completeInsertionSuccessfully()
 		}
 	}
+		
+	/**
+	 When we save and the instance is deallocated we do not want the
+	 completion block to be invoked.
+	 */
+	func test_save_doesNotDeliverDeletionErrorAfterSUTInstanceHasBeenDeallocated() {
+		let store = FeedStoreSpy()
+		var sut: LocalFeedLoader? = LocalFeedLoader(store: store, currentDate: Date.init)
+		
+		var receivedResults = [Error?]()
+		
+		sut?.save([uniqueItem()]) { receivedResults.append($0) }
+		
+		sut = nil
+		// complete with an error after sut has been deallocated
+		store.completeDeletion(with: anyNSError())
+		
+		XCTAssertTrue(receivedResults.isEmpty)
+	}
+	
 	
 	// MARK: - Helpers
 	private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
