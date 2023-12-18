a1d004a5d2b69c87fdca6555b932055f280c19d4
Valerio DAlessio <u105550@canda.com>
2023-12-18 16:56:38 +0100

    make FeedCach static and move to its own file

--- EssentialFeed/FeedCache/LocalFeedLoader.swift
+++ EssentialFeed/FeedCache/LocalFeedLoader.swift
@@ -7,23 +7,9 @@
 
 import Foundation
 
-private final class FeedCachePolicy {
-	private let calendar = Calendar(identifier: .gregorian)
-	
-	private var maxCacheAgeInDays: Int {
-		return 7
-	}
-	
-	func validate(_ timestamp: Date, against date: Date) -> Bool {
-		guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else { return false }
-		return date < maxCacheAge
-	}
-}
-
 public final class LocalFeedLoader: FeedLoader {
 	private let store: FeedStore
 	private let currentDate: () -> Date
-	private let cachePolicy = FeedCachePolicy()
 	
 	public init(store: FeedStore, currentDate: @escaping () -> Date) {
 		self.store = store
@@ -65,7 +51,7 @@ extension LocalFeedLoader {
 			switch result {
 			case .failure(let error):
 				completion(.failure(error))
-			case let .found(feed: feed, timestamp: timestamp) where self.cachePolicy.validate(timestamp, against: self.currentDate()):
+			case let .found(feed: feed, timestamp: timestamp) where FeedCachePolicy.validate(timestamp, against: self.currentDate()):
 				completion(.success(feed.toModels()))
 			case .found, .empty:
 				completion(.success([]))
@@ -83,7 +69,7 @@ extension LocalFeedLoader {
 			switch result {
 			case .failure:
 				self.store.deleteCachedFeed() { _ in }
-			case let .found(feed: _, timestamp: timestamp) where !self.cachePolicy.validate(timestamp, against: self.currentDate()):
+			case let .found(feed: _, timestamp: timestamp) where !FeedCachePolicy.validate(timestamp, against: self.currentDate()):
 				self.store.deleteCachedFeed() { _ in }
 			case .empty, .found:
 				break
