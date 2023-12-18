6421d700bf5a69b8ab76fd52713403f58a8de96f
Valerio DAlessio <u105550@canda.com>
2023-12-18 14:56:52 +0100

    validateCahce does not delete less than 7 days cache

--- EssentialFeedTests/FeedCache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeedTests/FeedCache/LoadFeedFromCacheUseCaseTests.swift
@@ -94,7 +94,7 @@ final class LoadFeedFromCacheUseCaseTests: XCTestCase {
 		XCTAssertEqual(store.receivedMessages, [.retrieve])
 	}
 	
-	func test_load_doesNotDeletesCacheOnLessThanSevenDaysOldCache() {
+	func test_load_hasNotSideEffectOnLessThanSevenDaysOldCache() {
 		let feed = uniqueImageFeed()
 		let fixedCurrentDate = Date()
 		let lessThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: 1)
--- EssentialFeedTests/FeedCache/ValidateFeedCacheUserCaseTests.swift
+++ EssentialFeedTests/FeedCache/ValidateFeedCacheUserCaseTests.swift
@@ -15,20 +15,30 @@ final class ValidateFeedCacheUserCaseTests: XCTestCase {
 		XCTAssertEqual(store.receivedMessages, [])
 	}
 	
-	func test_validate_deletesCacheOnRetrievalError() {
+	func test_validateCache_deletesCacheOnRetrievalError() {
 		let(sut, store) = makeSUT()
 		sut.validateCache()
 		store.completeRetrieval(with: anyNSError())
 		XCTAssertEqual(store.receivedMessages, [.retrieve, .deleteCacheFeed])
 	}
 	
-	func test_validate_doesNotDeletesCacheOnEmptyCache() {
+	func test_validateCache_doesNotDeletesCacheOnEmptyCache() {
 		let(sut, store) = makeSUT()
 		sut.validateCache()
 		store.completeWithEmptyCache()
 		XCTAssertEqual(store.receivedMessages, [.retrieve])
 	}
 
+	func test_validateCache_doesNotDeletesCacheOnLessThanSevenDaysOldCache() {
+		let feed = uniqueImageFeed()
+		let fixedCurrentDate = Date()
+		let lessThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: 1)
+		let(sut, store) = makeSUT() { fixedCurrentDate }
+		sut.validateCache()
+		store.completeRetrieval(with: feed.local, timestamp: lessThanSevenDaysOldTimestamp)
+		XCTAssertEqual(store.receivedMessages, [.retrieve])
+	}
+	
 	// MARK: - Helpers
 	private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
 		let store = FeedStoreSpy()
@@ -38,7 +48,42 @@ final class ValidateFeedCacheUserCaseTests: XCTestCase {
 		return (sut, store)
 	}
 	
+	private func uniqueImage() -> FeedImage {
+		return FeedImage(
+			id: .init(),
+			description: nil,
+			location: nil,
+			url: anyURL()
+		)
+	}
+	
+	private func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
+		let models = [uniqueImage(), uniqueImage()]
+		let localItems = models.map { LocalFeedImage(
+			id: $0.id,
+			description: $0.description,
+			location: $0.location,
+			url: $0.imageURL)
+		}
+		
+		return (models, localItems)
+	}
+	
 	private func anyNSError() -> NSError {
 		return NSError(domain: "any error", code: 0)
+	}
+	
+	private func anyURL() -> URL {
+		return URL(string: "http://any-url.com")!
+	}
+}
+
+private extension Date {
+	func adding(days: Int) -> Date {
+		return Calendar(identifier: .gregorian).date(byAdding: .init(day: days), to: self)!
+	}
+	
+	func adding(seconds: TimeInterval) -> Date {
+		return self + seconds
 	}
 }
