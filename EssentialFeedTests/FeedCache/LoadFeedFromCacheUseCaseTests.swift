ffe0c956374185eff338097a20f56ffc4d229d9d
Valerio DAlessio <u105550@canda.com>
2023-12-18 12:52:56 +0100

    Load delete seven days old cache upon retrieval

--- EssentialFeedTests/FeedCache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeedTests/FeedCache/LoadFeedFromCacheUseCaseTests.swift
@@ -104,6 +104,17 @@ final class LoadFeedFromCacheUseCaseTests: XCTestCase {
 		XCTAssertEqual(store.receivedMessages, [.retrieve])
 	}
 	
+	func test_load_doesDeletesCacheOnSevenDaysOldCache() {
+		let feed = uniqueImageFeed()
+		let fixedCurrentDate = Date()
+		let sevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7)
+		let(sut, store) = makeSUT() { fixedCurrentDate }
+		sut.load() { _ in }
+		
+		store.completeRetrieval(with: feed.local, timestamp: sevenDaysOldTimestamp)
+		XCTAssertEqual(store.receivedMessages, [.retrieve, .deleteCacheFeed])
+	}
+	
 	// MARK: - Helpers
 	private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
 		let store = FeedStoreSpy()
