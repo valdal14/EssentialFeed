689a391ff8ceea73dd44dc661294ae70b3454ce9
Valerio DAlessio <u105550@canda.com>
2023-12-18 16:59:37 +0100

    make the validate function also internal

--- EssentialFeed/FeedCache/FeedCachePolicy.swift
+++ EssentialFeed/FeedCache/FeedCachePolicy.swift
@@ -16,7 +16,7 @@ internal final class FeedCachePolicy {
 	
 	private init() {}
 	
-	static func validate(_ timestamp: Date, against date: Date) -> Bool {
+	internal static func validate(_ timestamp: Date, against date: Date) -> Bool {
 		guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else { return false }
 		return date < maxCacheAge
 	}
