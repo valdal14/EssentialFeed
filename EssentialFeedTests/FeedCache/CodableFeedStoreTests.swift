a0d50fa0ce2d6f1b1046b653c190d6e015d09234
Valerio DAlessio <u105550@canda.com>
2023-12-20 06:23:09 +0100

    add factory with memory leak tracking to create the SUT

--- EssentialFeedTests/FeedCache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/FeedCache/CodableFeedStoreTests.swift
@@ -76,7 +76,7 @@ final class CodableFeedStoreTests: XCTestCase {
 	}
 	
 	func test_retrieve_deliversEmptyOnEmptyCache() {
-		let sut = CodableFeedStore()
+		let sut = makeSUT()
 		
 		let exp = expectation(description: "Wait for cache retrieve completion to be done")
 		
@@ -95,7 +95,7 @@ final class CodableFeedStoreTests: XCTestCase {
 	}
 	
 	func test_retrieve_hasNoSideEffectsOnEmptyCache() {
-		let sut = CodableFeedStore()
+		let sut = makeSUT()
 		
 		let exp = expectation(description: "Wait for first and second cache retrieve completion to be done")
 		
@@ -116,7 +116,7 @@ final class CodableFeedStoreTests: XCTestCase {
 	}
 	
 	func test_retrieveAfterInsertingToEmptyCache_deliversInsertedValues() {
-		let sut = CodableFeedStore()
+		let sut = makeSUT()
 		let feed = uniqueImageFeed().local
 		let timestamp = Date()
 		let exp = expectation(description: "Wait for insert cache and retrieve completion to be done")
@@ -137,5 +137,12 @@ final class CodableFeedStoreTests: XCTestCase {
 		}
 		
 		wait(for: [exp], timeout: 1.0)
+	}
+	
+	// MARK: - Helpers
+	private func makeSUT(file: StaticString = #file, line: UInt = #line) -> CodableFeedStore {
+		let sut = CodableFeedStore()
+		trackForMemoryLeak(sut, file: file, line: line)
+		return sut
 	}
 }
