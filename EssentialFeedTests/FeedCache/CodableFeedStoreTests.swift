c56e59489a39b91e7e15ef01c0a60d0f864c2344
Valerio DAlessio <u105550@canda.com>
2023-12-21 06:31:40 +0100

    add a missing expect

--- EssentialFeedTests/FeedCache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/FeedCache/CodableFeedStoreTests.swift
@@ -92,6 +92,7 @@ final class CodableFeedStoreTests: XCTestCase {
 		let insertionError = insert((feed: feed, timestamp: timestamp), to: sut)
 		
 		XCTAssertNotNil(insertionError, "Expected cache insertion to fail with an error")
+		expect(sut, toRetrieve: .empty)
 	}
 	
 	func test_delete_hasNoSideEffectsOnEmptyCache() {
