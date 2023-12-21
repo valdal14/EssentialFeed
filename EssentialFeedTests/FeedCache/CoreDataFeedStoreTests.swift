d98a8682e501cbb37ffb95b357e9f0d8794848a0
Valerio DAlessio <u105550@canda.com>
2023-12-21 09:22:55 +0100

    create a CoreDataFeedStore and implement the assertThatRetrieveDeliversEmptyOnEmptyCache

--- EssentialFeedTests/FeedCache/CoreDataFeedStoreTests.swift
+++ EssentialFeedTests/FeedCache/CoreDataFeedStoreTests.swift
@@ -4,12 +4,29 @@
 //
 //  Created by Valerio D'ALESSIO on 21/12/23.
 //
-
 import XCTest
+@testable import EssentialFeed
+
+final class CoreDataFeedStore: FeedStore {
+	
+	func retrieve(completion: @escaping RetrievalCompletions) {
+		completion(.empty)
+	}
+	
+	func insert(_ feed: [EssentialFeed.LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+		
+	}
+	
+	func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+		
+	}
+}
+
 
 final class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
 	func test_retrieve_deliversEmptyOnEmptyCache() {
-			
+		let sut = makeSUT()
+		assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
 	}
 	
 	func test_retrieve_hasNoSideEffectsOnEmptyCache() {
@@ -56,4 +73,10 @@ final class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
 		
 	}
 	
+	// MARK: - Helpers
+	func makeSUT(file: StaticString = #file, line: UInt = #line) -> FeedStore {
+		let sut = CoreDataFeedStore()
+		trackForMemoryLeak(sut)
+		return sut
+	}
 }
