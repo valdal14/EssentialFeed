67ecc10f89b1b825942577989b98a2dc5563220f
Valerio DAlessio <u105550@canda.com>
2023-12-23 17:40:20 +0100

    add helpes dependencies and LocalFeedLoader in integration with CoreDataFeedStore delivers no items on empty cache

--- EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -6,9 +6,56 @@
 //
 
 import XCTest
+import EssentialFeed
 
 final class EssentialFeedCacheIntegrationTests: XCTestCase {
 
+	func test_load_deliversNoItemsOnEmptyCache() throws {
+		let sut = try makeSUT()
+		
+		let exp = expectation(description: "Wait for load completion")
+		sut.load { result in
+			switch result {
+			case .success(let imageFeed):
+				XCTAssertEqual(imageFeed, [], "Expected empty feed")
+			case .failure(let error):
+				XCTFail("Expected successful feed result but got \(error) instead")
+			}
+			
+			exp.fulfill()
+		}
+		
+		wait(for: [exp], timeout: 1.0)
+	}
 	
-
+	// MARK: - Helpers
+	private func makeSUT(file: StaticString = #file, line: UInt = #line) throws -> LocalFeedLoader {
+		let storeBundle = Bundle(for: CoreDataFeedStore.self)
+		let storeURL = testSpecificStoreURL()
+		let store = try CoreDataFeedStore(storeURL: storeURL, bundle: storeBundle)
+		let sut = LocalFeedLoader(store: store, currentDate: Date.init)
+		trackForMemoryLeak(store, file: file, line: line)
+		trackForMemoryLeak(sut, file: file, line: line)
+		return sut
+	}
+	
+	private func setupEmptyStoreState() {
+		deleteStoreArtifacts()
+	}
+	
+	private func undoStoreSideEffects() {
+		deleteStoreArtifacts()
+	}
+	
+	private func deleteStoreArtifacts() {
+		try? FileManager.default.removeItem(at: testSpecificStoreURL())
+	}
+	
+	private func testSpecificStoreURL() -> URL {
+		return cachesDirectory().appendingPathComponent("\(type(of: self)).store")
+	}
+	
+	private func cachesDirectory() -> URL {
+		return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+	}
 }
