aedd8fab6a2c1a4696ebca0e68bf89f0b0e5a56c
Valerio DAlessio <u105550@canda.com>
2023-12-23 17:22:46 +0100

    Add EssentialFeedCacheIntegrationTests target to separate the integration tests with the unit/isolated tests

---
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,14 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Valerio D'ALESSIO on 23/12/23.
+//
+
+import XCTest
+
+final class EssentialFeedCacheIntegrationTests: XCTestCase {
+
+	
+
+}
