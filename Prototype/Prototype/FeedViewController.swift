84432a87dde4315aaa9b8b4e01b01df261251921
Valerio DAlessio <u105550@canda.com>
2023-12-27 22:23:15 +0100

    add mocked data for the prototype

--- Prototype/Prototype/FeedViewController.swift
+++ Prototype/Prototype/FeedViewController.swift
@@ -7,10 +7,17 @@
 
 import UIKit
 
+struct FeedImageViewModel {
+	let description: String?
+	let location: String?
+	let imageName: String
+}
+
 class FeedViewController: UITableViewController {
+	let viewModel: [FeedImageViewModel] = FeedImageViewModel.prototypeFeed
 	
 	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return 10
+		return viewModel.count
 	}
 	
 	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
