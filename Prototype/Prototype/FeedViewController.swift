825695fe05de33f3901d86829408b212deffe59b
Valerio DAlessio <u105550@canda.com>
2023-12-29 13:10:13 +0100

    updated protype

--- Prototype/Prototype/FeedViewController.swift
+++ Prototype/Prototype/FeedViewController.swift
@@ -14,7 +14,42 @@ struct FeedImageViewModel {
 }
 
 class FeedViewController: UITableViewController {
-	let feed: [FeedImageViewModel] = FeedImageViewModel.prototypeFeed
+	private var feed: [FeedImageViewModel] = []
+	
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		
+		tableView.setContentOffset(
+			.init(
+				x: 0,
+				y: -tableView.contentInset.top
+			), animated: false
+		)
+	}
+	
+	/**
+	 Fix for the refreshControl is not working on iOS 16.1 and above
+	 https://academy.essentialdeveloper.com/courses/1112681/lectures/50273453
+	 */
+	override func viewIsAppearing(_ animated: Bool) {
+		refresh()
+	}
+	
+	@IBAction func refresh() {
+		refreshControl?.beginRefreshing()
+		
+		DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+			if self.feed.isEmpty {
+				self.feed = FeedImageViewModel.prototypeFeed
+				self.tableView.reloadData()
+			}
+			self.refreshControl?.endRefreshing()
+		}
+	}
+}
+
+// MARK: - FeedViewController - UITableView Helpers
+extension FeedViewController {
 	
 	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 		return feed.count
