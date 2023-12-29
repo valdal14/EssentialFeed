80f6ed2edde32125ec19ced2bf4892903bc1a812
Valerio DAlessio <u105550@canda.com>
2023-12-29 08:13:01 +0100

    fix issue with targets and schemes

---
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewControllerTests.swift
+//  EssentialFeediOSTests
+//
+//  Created by Valerio D'ALESSIO on 29/12/23.
+//
+
+import EssentialFeed
+import XCTest
+import UIKit
+
+final class FeedViewControllerTests: XCTestCase {}
