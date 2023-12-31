cd41021c9105fbad9634786ea0f864e7e25e3439
Valerio DAlessio <u105550@canda.com>
2023-12-31 10:25:21 +0100

    Extract cancelImageDataLoad(from: URL) method from `FeedImageDataLoader` protocol into a new `FeedImageDataLoaderTask` protocol that represents a task that can be cancelled. This way, we respect the Interface Segregation Principle and `FeedImageDataLoader` implementations are not forced to be stateful.

--- EssentialFeediOS/FeedViewController.swift
+++ EssentialFeediOS/FeedViewController.swift
@@ -8,9 +8,26 @@
 import EssentialFeed
 import UIKit
 
+/**
+ Instead of having an interface with two methods ( breaking the ISP )
+ we could allow the client to decide when to cancel. As we can see here
+ we need to keep two states one for the loaded images and one for the
+ cancelled and we could move this responsability to the client ( FeedViewController ). We could
+ return a task that can be cancelled. Doing so we can have only one method
+ that return a cancellable task and we leave the state management to the client
+ 
+ public protocol FeedImageDataLoader {
+	 func loadImageData(from url: URL)
+	 func cancelImageDataLoad(from url: URL)
+ }
+ */
+
+public protocol FeedImageDataLoaderTask {
+	func cancel()
+}
+
 public protocol FeedImageDataLoader {
-	func loadImageData(from url: URL)
-	func cancelImageDataLoad(from url: URL)
+	func loadImageData(from url: URL) -> FeedImageDataLoaderTask
 }
 
 final public class FeedViewController: UITableViewController {
@@ -18,6 +35,7 @@ final public class FeedViewController: UITableViewController {
 	private var imageLoader: FeedImageDataLoader?
 	private var isViewAppeared = false
 	private var tableModel: [FeedImage] = []
+	private var tasks: [IndexPath : FeedImageDataLoaderTask] = [:]
 	
 	public convenience init(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) {
 		self.init()
@@ -65,12 +83,12 @@ public extension FeedViewController {
 		cell.locationContainer.isHidden = (cellModel.location != nil) ? false : true
 		cell.locationLabel.text = cellModel.location
 		cell.descriptionLabel.text = cellModel.description
-		imageLoader?.loadImageData(from: cellModel.imageURL)
+		tasks[indexPath] = imageLoader?.loadImageData(from: cellModel.imageURL)
 		return cell
 	}
 	
 	override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-		let cellModel = tableModel[indexPath.row]
-		imageLoader?.cancelImageDataLoad(from: cellModel.imageURL)
+		tasks[indexPath]?.cancel()
+		tasks[indexPath] = nil
 	}
 }
--- EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -230,12 +230,16 @@ final class FeedViewControllerTests: XCTestCase {
 		private(set) var loadedImageURLs: [URL] = []
 		private(set) var cancelImageURLs: [URL] = []
 		
-		func loadImageData(from url: URL) {
-			loadedImageURLs.append(url)
+		private struct TaskSpy: FeedImageDataLoaderTask {
+			let cancelCallback: () -> Void
+			func cancel() {
+				cancelCallback()
+			}
 		}
 		
-		func cancelImageDataLoad(from url: URL) {
-			cancelImageURLs.append(url)
+		func loadImageData(from url: URL) -> FeedImageDataLoaderTask {
+			loadedImageURLs.append(url)
+			return TaskSpy { [weak self] in self?.cancelImageURLs.append(url) }
 		}
 	}
 }
