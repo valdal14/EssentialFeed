ac3557bc490cdb87e0798bad1ecdb4b6a304dfb6
Valerio DAlessio <u105550@canda.com>
2024-01-05 16:04:00 +0100

    Moved `FeedImageDataLoaderPresentationAdapter` to a separate file likewise the `WeakRefVirtualProxy`

--- EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -26,25 +26,7 @@ public final class FeedUIComposer {
 	}
 }
 
-private final class WeakRefVirtualProxy<T: AnyObject> {
-	private weak var object: T?
-	
-	init(_ object: T) {
-		self.object = object
-	}
-}
 
-extension WeakRefVirtualProxy: FeedLoadingView where T: FeedLoadingView {
-	func display(_ viewModel: FeedLoadingViewModel) {
-		object?.display(viewModel)
-	}
-}
-
-extension WeakRefVirtualProxy: FeedImageView where T: FeedImageView, T.Image == UIImage {
-	func display(_ model: FeedImageViewModel<UIImage>) {
-		object?.display(model)
-	}
-}
 
 private final class FeedViewAdapter: FeedView {
 	private weak var controller: FeedViewController?
@@ -89,38 +71,6 @@ private final class FeedLoaderPresentationAdapter: FeedViewControllerDelegate {
 				self?.presenter?.didFinishLoadingFeed(with: error)
 			}
 		}
-	}
-}
-
-private final class FeedImageDataLoaderPresentationAdapter<View: FeedImageView, Image>: FeedImageCellControllerDelegate where View.Image == Image {
-	private let model: FeedImage
-	private let imageLoader: FeedImageDataLoader
-	private var task: FeedImageDataLoaderTask?
-	
-	var presenter: FeedImagePresenter<View, Image>?
-	
-	init(model: FeedImage, imageLoader: FeedImageDataLoader) {
-		self.model = model
-		self.imageLoader = imageLoader
-	}
-	
-	func didRequestImage() {
-		presenter?.didStartLoadingImageData(for: model)
-		
-		let model = self.model
-		task = imageLoader.loadImageData(from: model.imageURL) { [weak self] result in
-			switch result {
-			case let .success(data):
-				self?.presenter?.didFinishLoadingImageData(with: data, for: model)
-				
-			case let .failure(error):
-				self?.presenter?.didFinishLoadingImageData(with: error, for: model)
-			}
-		}
-	}
-	
-	func didCancelImageRequest() {
-		task?.cancel()
 	}
 }
 
