1970eb691ef8a14ac6a54ebcef262af8b7526614
Valerio DAlessio <u105550@canda.com>
2024-01-05 16:29:55 +0100

    Moved UIImage helpers into a separate file

--- EssentialFeediOSTests/Feed UI/FeedUIIntegrationTests.swift
+++ EssentialFeediOSTests/Feed UI/FeedUIIntegrationTests.swift
@@ -357,17 +357,3 @@ final class FeedUIIntegrationTests: XCTestCase {
 		return UIImage.make(withColor: .red).pngData()!
 	}
 }
-
-// MARK: - UIImage extension
-extension UIImage {
-	static func make(withColor color: UIColor) -> UIImage {
-		let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
-		let format = UIGraphicsImageRendererFormat()
-		format.scale = 1
-		
-		return UIGraphicsImageRenderer(size: rect.size, format: format).image { rendererContext in
-			color.setFill()
-			rendererContext.fill(rect)
-		}
-	}
-}
