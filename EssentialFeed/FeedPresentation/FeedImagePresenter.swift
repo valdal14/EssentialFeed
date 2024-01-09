7b6a0fc7a5617f323b8341e68e6dcfe81ed6e589
Valerio DAlessio <u105550@canda.com>
2024-01-09 11:31:56 +0100

    moved the `FeedImagePresenter` to production code

---
+++ EssentialFeed/FeedPresentation/FeedImagePresenter.swift
@@ -0,0 +1,68 @@
+//
+//  FeedImagePresenter.swift
+//  EssentialFeed
+//
+//  Created by Valerio D'ALESSIO on 9/1/24.
+//
+
+import Foundation
+
+public struct FeedImageViewModel<Image> {
+	public let description: String?
+	public let location: String?
+	public let image: Image?
+	public let isLoading: Bool
+	public let shouldRetry: Bool
+
+	public var hasLocation: Bool {
+		return location != nil
+	}
+}
+
+public protocol FeedImageView {
+	associatedtype Image
+
+	func display(_ model: FeedImageViewModel<Image>)
+}
+
+public final class FeedImagePresenter<View: FeedImageView, Image> where View.Image == Image {
+	private let view: View
+	private let imageTransformer: (Data) -> Image?
+	private struct InvalidImageDataError: Error {}
+	
+	public init(view: View, imageTransformer: @escaping (Data) -> Image?) {
+		self.view = view
+		self.imageTransformer = imageTransformer
+	}
+	
+	public func didStartLoadingImageData(for model: FeedImage) {
+		view.display(FeedImageViewModel(
+			description: model.description,
+			location: model.location,
+			image: nil,
+			isLoading: true,
+			shouldRetry: false))
+	}
+	
+	public func didFinishLoadingImageData(with data: Data, for model: FeedImage) {
+		guard let image = imageTransformer(data) else {
+			return didFinishLoadingImageData(with: InvalidImageDataError(), for: model)
+		}
+		
+		view.display(FeedImageViewModel(
+			description: model.description,
+			location: model.location,
+			image: image,
+			isLoading: false,
+			shouldRetry: false))
+	}
+	
+	public func didFinishLoadingImageData(with error: Error, for model: FeedImage) {
+		view.display(FeedImageViewModel(
+			description: model.description,
+			location: model.location,
+			image: nil,
+			isLoading: false,
+			shouldRetry: true))
+	}
+}
--- EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
+++ EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -8,66 +8,6 @@
 import EssentialFeed
 import XCTest
 
-struct FeedImageViewModel<Image> {
-	let description: String?
-	let location: String?
-	let image: Image?
-	let isLoading: Bool
-	let shouldRetry: Bool
-
-	var hasLocation: Bool {
-		return location != nil
-	}
-}
-
-protocol FeedImageView {
-	associatedtype Image
-
-	func display(_ model: FeedImageViewModel<Image>)
-}
-
-final class FeedImagePresenter<View: FeedImageView, Image> where View.Image == Image {
-	private let view: View
-	private let imageTransformer: (Data) -> Image?
-	private struct InvalidImageDataError: Error {}
-	
-	init(view: View, imageTransformer: @escaping (Data) -> Image?) {
-		self.view = view
-		self.imageTransformer = imageTransformer
-	}
-	
-	func didStartLoadingImageData(for model: FeedImage) {
-		view.display(FeedImageViewModel(
-			description: model.description,
-			location: model.location,
-			image: nil,
-			isLoading: true,
-			shouldRetry: false))
-	}
-	
-	func didFinishLoadingImageData(with data: Data, for model: FeedImage) {
-		guard let image = imageTransformer(data) else {
-			return didFinishLoadingImageData(with: InvalidImageDataError(), for: model)
-		}
-		
-		view.display(FeedImageViewModel(
-			description: model.description,
-			location: model.location,
-			image: image,
-			isLoading: false,
-			shouldRetry: false))
-	}
-	
-	func didFinishLoadingImageData(with error: Error, for model: FeedImage) {
-		view.display(FeedImageViewModel(
-			description: model.description,
-			location: model.location,
-			image: nil,
-			isLoading: false,
-			shouldRetry: true))
-	}
-}
-
 final class FeedImagePresenterTests: XCTestCase {
 
 	func test_load_doesNotFireAnyMessages() {
