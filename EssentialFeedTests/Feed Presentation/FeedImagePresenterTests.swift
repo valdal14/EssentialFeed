786e7ebae783e23bc3f6ae20e850e04ae3404b43
Valerio DAlessio <u105550@canda.com>
2024-01-09 11:28:50 +0100

    add InvalidImageDataError type

--- EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
+++ EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -29,7 +29,8 @@ protocol FeedImageView {
 final class FeedImagePresenter<View: FeedImageView, Image> where View.Image == Image {
 	private let view: View
 	private let imageTransformer: (Data) -> Image?
-
+	private struct InvalidImageDataError: Error {}
+	
 	init(view: View, imageTransformer: @escaping (Data) -> Image?) {
 		self.view = view
 		self.imageTransformer = imageTransformer
@@ -45,13 +46,16 @@ final class FeedImagePresenter<View: FeedImageView, Image> where View.Image == I
 	}
 	
 	func didFinishLoadingImageData(with data: Data, for model: FeedImage) {
-		let image = imageTransformer(data)
+		guard let image = imageTransformer(data) else {
+			return didFinishLoadingImageData(with: InvalidImageDataError(), for: model)
+		}
+		
 		view.display(FeedImageViewModel(
 			description: model.description,
 			location: model.location,
 			image: image,
 			isLoading: false,
-			shouldRetry: image == nil))
+			shouldRetry: false))
 	}
 	
 	func didFinishLoadingImageData(with error: Error, for model: FeedImage) {
