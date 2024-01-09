24ab27e02ca7639594fc30d3ba2cc9520ec3b00a
Valerio DAlessio <u105550@canda.com>
2024-01-09 10:05:17 +0100

    extract SUT creation to a helper factory function

--- EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
+++ EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -14,9 +14,16 @@ final class FeedImagePresenter {
 final class FeedImagePresenterTests: XCTestCase {
 
 	func test_load_doesNotFireAnyMessages() {
+		let (_, view) = makeSUT()
+		XCTAssertEqual(view.messages, [])
+	}
+	// MARK: - Helpers
+	private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedImagePresenter, view: ViewSpy) {
 		let view = ViewSpy()
 		let sut = FeedImagePresenter()
-		XCTAssertEqual(view.messages, [])
+		trackForMemoryLeak(view, file: file, line: line)
+		trackForMemoryLeak(sut, file: file, line: line)
+		return (sut, view)
 	}
 	
 	private final class ViewSpy {
